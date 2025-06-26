a9dc9c026d5ce2555109aba78a3d8734828efd39
Mahesh Mhetre <mhetrem15@gmail.com>
2025-06-26 16:02:56 +0530

    adding linkks and script

---
+++ script.js
@@ -0,0 +1,17 @@
+function downloadFile(url, filename) {
+    fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = filename;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            URL.revokeObjectURL(link.href);
+        })
+        .catch(error => {
+            console.error('Download failed:', error);
+            window.open(url, '_blank');
+        });
+}
